b0fd123d891aaf66944e01775d0ac47f6ac82022
Ondrej Podsztavek <ondrej.podsztavek@gmail.com>
2016-11-08 20:55:01 +0100

    Loop interchange

--- optimize.cc
+++ optimize.cc
@@ -16,68 +16,62 @@ uint32_t **read_optimize(FILE *file, uint32_t &num) {
      * then there in num of lines each containing three numbers
      * a, b, c separated by spaces
      */
-    uint32_t **arr = new uint32_t*[num];
-    if (arr == nullptr)
+    uint32_t **lgs = new uint32_t*[num];
+    if (lgs == nullptr)
         return nullptr;
     for (uint32_t i = 0; i < num; ++i) {
-        arr[i] = new uint32_t[3];
-        if (arr[i] == nullptr)
+        lgs[i] = new uint32_t[7];
+        if (lgs[i] == nullptr)
             return nullptr;
         if (fscanf(file, "%" SCNu32 "%" SCNu32 "%" SCNu32,
-                    arr[i], arr[i] + 1, arr[i] + 2) != 3)
+                    lgs[i], lgs[i] + 1, lgs[i] + 2) != 3)
             return nullptr;
+        /* x */
+        lgs[i][3] = 0;
+        /* count in interval */
+        lgs[i][4] = 0;
+        /* minimal distance */
+        lgs[i][5] = UINT32_MAX;
+        /* maximal distance */
+        lgs[i][6] = 0;
     }
 
-    return arr;
+    return lgs;
 }
 
 double optimize(FILE *input, uint32_t k, uint32_t c, uint32_t d, uint32_t e) {
     /* read linear generators */
     uint32_t num;
-    uint32_t **linear_generators = read_optimize(input, num);
-    if (linear_generators == nullptr) {
+    uint32_t **lgs = read_optimize(input, num);
+    if (lgs == nullptr) {
         fprintf(stdout, "Input file reading error.\n");
         return 0;
     }
+
+    uint32_t dist;
 
     std::chrono::high_resolution_clock::time_point start, end;
     /* start time measurement */
     start = std::chrono::high_resolution_clock::now();
 
-    uint32_t a, b, n;
-    uint32_t x;
-    uint32_t count_in_interval;
-    uint32_t min_distance, max_distance;
-    uint32_t distance;
-    /*
-     * TODO
-     * tady prohodit cykly takze pujdu k vnejsi a pres vsechny generatory
-     * vnitrni tim to budu moct vektorizovat akorat to nebude vsechno v
-     * registrech
-     */
-    /* for each linear generator */
-    for (uint32_t i = 0; i < num; ++i) {
-        a = linear_generators[i][0];
-        b = linear_generators[i][1];
-        n = linear_generators[i][2];
-        x = 0;
-        count_in_interval = 0;
-        min_distance = UINT_MAX;
-        max_distance = 0;
+    for (uint32_t i = 0; i < k; ++i) {
+        for (uint32_t j = 0; j < num; ++j) {
+            lgs[j][3] = (
+                    lgs[j][0] * lgs[j][3] + lgs[j][1]) % (2 << (lgs[j][2] - 1)
+                        );
 
-        for (uint32_t j = 0; j < k; ++j) {
-            x = ((a * x + b) % (2 << (n - 1)));
-            count_in_interval += (c <= x && x <= d) ? 1 : 0;
-            /* TODO pouzit ten rozepsanej z prednasky */
-            distance = __builtin_popcount(x ^ e);
-            min_distance = std::min(min_distance, distance);
-            max_distance = std::max(max_distance, distance);
+            lgs[j][4] += (c <= lgs[j][3] && lgs[j][3] <= d) ? 1 : 0;
+
+            /* population count */
+            dist = lgs[j][3] ^ e;
+            dist = dist - ((dist >> 1) & 0x55555555);
+            dist = (dist & 0x33333333) + ((dist >> 2) & 0x33333333);
+            dist = (((dist + (dist >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+
+            lgs[j][5] = std::min(lgs[j][5], dist);
+            lgs[j][6] = std::max(lgs[j][6], dist);
         }
 
-        /* use computed values so compiler does not exclude them */
-        fprintf(stderr,
-                "%" PRIu32 "%" PRIu32 "%" PRIu32,
-                count_in_interval, min_distance, max_distance);
     }
 
     /* end time measurement */
@@ -88,10 +82,16 @@ double optimize(FILE *input, uint32_t k, uint32_t c, uint32_t d, uint32_t e) {
             end - start
             );
 
+    /* use computed values so compiler does not exclude them */
+    for (uint32_t i = 0; i < num; ++i)
+        fprintf(stderr,
+                "%" PRIu32 "%" PRIu32 "%" PRIu32,
+                lgs[i][4], lgs[i][5], lgs[i][6]);
+
     /* free array with linear generators */
     for (uint32_t i = 0; i < num; ++i)
-        delete [] linear_generators[i];
-    delete [] linear_generators;
+        delete [] lgs[i];
+    delete [] lgs;
 
     return time_span.count();
 }
